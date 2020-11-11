509241150b03b7fe832f2f5c2f6a7f9f23ae3a45
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-11 15:48:53 +0900

    [FIX] component #11 동영상 토글 이슈 해결

--- js/showVideo.js
+++ js/showVideo.js
@@ -1,10 +1,16 @@
-const showVideoBtn = document.querySelector('#showVideoBtn');
-const video = document.querySelector('#video');
+// const showVideoBtn = document.querySelector('#showVideoBtn');
+// const video = document.querySelector('#video');
 
-showVideoBtn.addEventListener('click', () => {
-  if (video.style.display == 'none') {
-    video.style.display = 'block';
-  } else {
-    video.style.display = 'none';
-  }
+// showVideoBtn.addEventListener('click', () => {
+//   if (video.style.display == 'none') {
+//     video.style.display = 'block';
+//   } else {
+//     video.style.display = 'none';
+//   }
+// });
+
+$('.showVideoBtn').click(function() {
+  console.log('clicked');
+  console.log($(this).parent().parent().next());
+  $(this).parent().parent().next().toggle('.video');
 });
